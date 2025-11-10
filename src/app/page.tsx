e0a0945ef173c24af1706cab3de96901f27fa842
Reno Raksi <adriano.raksi@gmail.com>
2025-11-10 00:42:51 -0600

    Implement skeleton UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,28 @@ export default function Home() {
             </button>
           </form>
         </section>
+        <hr className="border-foreground/30" />
+        <section className="space-y-3">
+          <div>
+            <p className="text-sm text-foreground/60">Strategy</p>
+            <h2 className="text-3xl font-bold">Newest</h2>
+          </div>
+          <p>Showing current holdings as of Mon Nov 10, 2025 12:10 AM</p>
+          <div className="max-w-lg border border-solid border-foreground/10 rounded-xs p-8 space-y-6">
+            <div>
+              <p className="text-sm text-foreground/60">Allocation</p>
+              <h3 className="text-2xl">SPMO mix</h3>
+            </div>
+            <div className="grid grid-cols-2 gap-4 text-lg">
+              <p className="font-bold">Holdings</p>
+              <p className="font-bold">Distributions</p>
+              <p>GLD</p>
+              <p>25%</p>
+              <p>QLD</p>
+              <p>75%</p>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
